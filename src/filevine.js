557485e6ece61b382b57beb5fd82a2cfd9733cf6
Md Saddam <mdsonu7860@outlook.com>
2026-01-15 15:34:57 +0530

    Enable dotenv configuration and comment out previous setup

--- src/filevine.js
+++ src/filevine.js
@@ -1,10 +1,11 @@
-// import "dotenv/config";
+import "dotenv/config";
 
-import dotenv from "dotenv";
-import path from "path";
-dotenv.config({
-  path: path.resolve(process.cwd(), ".env"),
-});
+// import dotenv from "dotenv";
+// dotenv.config();
+// import path from "path";
+// dotenv.config({
+//   path: path.resolve(process.cwd(), ".env"),
+// });
 
 import { app } from "./app.js";
 import { logger } from "./index.js";
@@ -13,6 +14,8 @@ import { startFilevinePolling } from "./schedulers/filevine.poller.js";
 // import { hubspotToFilevine } from "./controllers/hubspotToFilevine.controller.js";
 
 logger.info(`CWD: ${process.cwd()}`);
+logger.info(`NODE_ENV: ${process.env.NODE_ENV}`);
+logger.info(`HS : ${process.env.HUBSPOT_API_KEY}`);
 
 const PORT = process.env.PORT || 5000;
 
