d06f11a7a62e63fd56d9d422f1adb4e89a280a4e
Md Saddam <mdsonu7860@outlook.com>
2026-01-15 16:01:43 +0530

    Refactor /run-manually endpoint to use async/await and add error handling

--- src/app.js
+++ src/app.js
@@ -6,7 +6,16 @@ const app = express();
 app.get("/", (req, res) => {
   res.status(200).send("health is Good");
 });
-app.get("/run-manually", hubspotToFilevine);
+app.get("/run-manually", async (req, res) => {
+  try {
+    console.log("Manual Trigger is started");
+    await hubspotToFilevine();
+
+    res.status(200).send("Manual Trigger is completed");
+  } catch (error) {
+    console.error("Error running manually:", error);
+  }
+});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
