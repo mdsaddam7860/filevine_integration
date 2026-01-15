3326a96d7b81ae614d28579681607bab9b977e78
Md Saddam <mdsonu7860@outlook.com>
2026-01-15 13:59:21 +0530

    Update dotenv configuration path in filevine.js

--- src/filevine.js
+++ src/filevine.js
@@ -1,12 +1,12 @@
-import "dotenv/config";
 import dotenv from "dotenv";
 import path from "path";
+dotenv.config({
+  path: path.resolve(process.cwd(), ".env"),
+});
+
 import { app } from "./app.js";
 import { logger } from "./index.js";
 
-dotenv.config({
-  path: path.resolve(process.cwd(), "../.env"),
-});
 import { startFilevinePolling } from "./schedulers/filevine.poller.js";
 // import { hubspotToFilevine } from "./controllers/hubspotToFilevine.controller.js";
 
